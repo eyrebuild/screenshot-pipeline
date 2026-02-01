d004839ecad91623d91a5dad631cfbbde2270c04
eyrebuild <nothanks@fake.com>
2026-02-01 16:44:49 +0000

    fix for running in git actions

--- src/scripts/screenshot.js
+++ src/scripts/screenshot.js
@@ -1,6 +1,13 @@
 import puppeteer from 'puppeteer';
 
-const browser = await puppeteer.launch();
+const browser = await puppeteer.launch({
+  args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-dev-shm-usage',
+    '--disable-gpu',
+  ],
+});
 const page = await browser.newPage();
 await page.goto('https://eyrebuild.github.io/screenshot-pipeline/', {
   waitUntil: 'networkidle2',
